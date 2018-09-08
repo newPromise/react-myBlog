83ae5810246b8a9462fec1f9e029bfbec2ce86a5
newPromise <250621458@qq.com>
2018-09-08 11:48:05 +0800

    完成日历组件 优化date函数

--- src/common/js/date.js
+++ src/common/js/date.js
@@ -1,7 +1,7 @@
 const REGEX_DATE = /^(\d{4})[\/ | -]?(\d{1,2})[\/ | -]?(\d{0,2})(.*?(\d{1,2}):(\d{1,2}):(\d{1,2}))?.?(\d{1,3})?$/;
 // const REGEX_DATE_TWO = /^(\d{4})-?(\d{1,2})-?(\d{0,2})(.*?(\d{1,2}):(\d{1,2}):(\d{1,2}))?.?(\d{1,3})?$/;
 
-const dateParse = (date) => {
+const dateParse = (date, monthIndex = 0) => {
     if (date === null) return new Date(NaN);
     if (date === void 0) return new Date();
     if (date instanceof Date) return date;
@@ -9,33 +9,60 @@ const dateParse = (date) => {
         // 匹配两种格式 "2018-09-01" or "2018/09/01"
         let reg = date.match(REGEX_DATE);
         if (reg) {
-            return new Date(reg[1], reg[2] - 1, reg[3], reg[5] || 0, reg[6] || 0, reg[7] || 0);
+            return new Date(reg[1], reg[2] - 1 + monthIndex, reg[3], reg[5] || 0, reg[6] || 0, reg[7] || 0);
         }
     }
 }
 
 
 class dateCl {
-    constructor(date) {
-        this.parse(date);
+    constructor(date, monthIndex) {
+        this.parse(date, monthIndex);
     }
-    parse(date) {
-        this.date = dateParse(date);
+    parse(date, monthIndex) {
+        this.$date = dateParse(date, monthIndex);
         this.init();
     }
     init() {
-        this.Y = this.date.getFullYear();
-        this.M = this.date.getMonth();
-        this.D = this.date.getDate();
-        this.W = this.date.getDay();
-        this.h = this.date.getHours();
-        this.m = this.date.getMinutes();
-        this.s = this.date.getSeconds();
+        this.$Y = this.$date.getFullYear();
+        this.$M = this.$date.getMonth();
+        this.$D = this.$date.getDate();
+        this.$W = this.$date.getDay();
+        this.$h = this.$date.getHours();
+        this.$m = this.$date.getMinutes();
+        this.$s = this.$date.getSeconds();
+    }
+    year() {
+        return this.$Y;
+    }
+    month() {
+        return this.$M + 1;
+    }
+    // return day in week
+    weekDay() {
+        return this.$W;
+    }
+    // return day in month
+    date() {
+        return this.$D;
+    }
+    // get this month day nums
+    getDays() {
+        return new Date(this.$Y, this.$M + 1, 0).getDate();
+    }
+    hour() {
+        return this.$h
+    }
+    minute() {
+        return this.$m
+    }
+    second() {
+        return this.$s
     }
 }
 
-let date = function (d) {
-    return new dateCl(d);
+let date = function (d, monthIndex) {
+    return new dateCl(d, monthIndex);
 }
 
 export default date;
---
+++ src/components/DatePicker/DatePicker.js
@@ -0,0 +1,120 @@
+import React , { Component } from "react";
+import style from "./DatePicker.less";
+import dateUtil from "../../common/js/date.js"
+
+class DatePicker extends Component {
+    constructor(props) {
+        super(props);
+        this.selectDate = this.selectDate.bind(this);
+        this.initDateDatas = this.initDateDatas.bind(this);
+        this.state = {
+            activeIndex: 0,
+            dateDatas: [],
+            dateYear: "",
+            dateMonth: "",
+            dateDay: "",
+            weekDay: "",
+            monthDays: ""
+        };
+    }
+    selectDate(tdIndex, cellDisabled) {
+        if (cellDisabled) {
+            return;
+        }
+        this.setState({ activeIndex: tdIndex });
+    }
+    componentDidMount() {
+        this.initDateDatas(this.props.date);
+    }
+    initDateDatas(date) {
+        const dateDatas = new Array(42);
+        const dateDay = dateUtil(date).date();
+        const year = dateUtil(date).year();
+        const month = dateUtil(date).month();
+        let weekDay = dateUtil(date).weekDay();
+        if (weekDay === 0) {
+            weekDay = 7;
+        }
+        const monthDayNum = dateUtil(date).getDays();
+        const lastMonthDays = dateUtil(date, -1).getDays();
+        for (let dayIndex = 0; dayIndex < dateDatas.length; dayIndex ++) {
+            if (dayIndex + 1 >= weekDay && ((dayIndex + 1) < (monthDayNum + weekDay))) {
+                dateDatas[dayIndex] = dayIndex + 2 - weekDay;
+            }
+            if (dayIndex + 1 < weekDay ) {
+                dateDatas[dayIndex] = lastMonthDays - weekDay + 2 + dayIndex;
+            }
+            if (((dayIndex + 1) >= (monthDayNum + weekDay))) {
+                dateDatas[dayIndex] = dayIndex - monthDayNum - weekDay + 2;
+            }
+        }
+        this.setState({ dateDatas, dateYear: year, dateMonth: month, dateDay, weekDay, monthDays: monthDayNum });
+    }
+    render() {
+        const DateHeader = () => {
+            return (
+                <div className= { style["blog-date-header"] }>
+                    <div className = { style["blog-header-inner"] }>
+                        <a className = { style["date-pev-btn"] }>日期</a>
+                        <span className = { style["header-date-text"] }>{ `${this.state.dateYear}年${this.state.dateMonth}月${this.state.dateDay}日` }</span>
+                        <a className = { style["date-next-btn"] }>日期</a>
+                    </div>
+                </div>
+            );
+        };
+        const DateBody = () => {
+            let theader = [];
+            let tbodyer = [];
+            let tdIndex = 0;
+            const TABLE_ROW = 6;
+            const TABLE_COL = 7;
+            const weeks = ["一", "二", "三", "四", "五", "六", "日"];
+            const allDateDays = [];
+            theader = weeks.reduce((pev, cur, index) => pev.push(<th  key={ index }>{cur}</th>) && pev, []);
+            for (let rowIndex = 0; rowIndex < TABLE_ROW; rowIndex++) {
+                let colEl = null;
+                const tdEls = [];
+                for (let colIndex = 0; colIndex < TABLE_COL; colIndex++) {
+                    const cellDisabled = tdIndex < this.state.weekDay - 1 || tdIndex + 1 >= this.state.monthDays + this.state.weekDay;
+                    tdEls.push(<td align="center" valign="middle" onClick={ this.selectDate.bind(this, tdIndex, cellDisabled) }  key={ colIndex }>
+                        <div className= { cellDisabled ? style["blog-date-disabledCell"] : (this.state.activeIndex === tdIndex ? style["blog-date-active-dayCell"] : style["blog-date-dayCell"]) }>{ this.state.dateDatas[tdIndex] }</div>
+                    </td>);
+                    tdIndex++;
+                }
+                colEl = <tr key= { rowIndex }>{ tdEls }</tr>;
+                tbodyer.push(colEl);
+            }
+            const Table = () => {
+                return (
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    { theader }
+                                </tr>
+                            </thead>
+                            <tbody>
+                                { tbodyer }
+                            </tbody>
+                        </table>
+                    </div>
+                );
+            };
+            return (
+                <div className={ style["blog-date-body"] }>
+                    <Table/>
+                </div>
+            )
+        };
+        return (
+            <div className= { style["blog-date-wrapper"] }>
+                <div className="blog-date-content">
+                    <DateHeader/>
+                    <DateBody/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DatePicker;
